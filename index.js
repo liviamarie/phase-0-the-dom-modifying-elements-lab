f65fe3f1d97a7f551ea6aa80065f6bffe413fac6
olivia <ojeglum@gmail.com>
2021-09-16 12:31:41 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+const newHeader = document.createElement("h1");
+document.body.appendChild(newHeader);
+newHeader.id = "victory";
+document.querySelector("h1#victory");
+newHeader.innerHTML = "Olivia is the champion";
+
+mainWithMainId = document.querySelector("#main").remove();
